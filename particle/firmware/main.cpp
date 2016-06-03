f60738a162762e980fbfb69991e70657d9fd2c19
Nathan Robinson <nrobinson2000@me.com>
2016-06-03 21:11:40 +0100

    21:11 of 03-06-2016

--- particle/firmware/main.cpp
+++ particle/firmware/main.cpp
@@ -1,12 +1,24 @@
 #include "application.h"
-STARTUP(WiFi.selectAntenna(ANT_AUTO));
+#include <IRremote.h>
+
+// STARTUP(WiFi.selectAntenna(ANT_AUTO));
+
+
+int RECV_PIN = D0;
+int ledpin = D2;
+bool flipping = false;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
 
 // 1XX 1XX 1XX 1XX
 // fl - fr - bl - br
 // 1 = Move forward; 2 = Move backwards; 0 = Don't move
 // XX = speed / 5 ; if XX == 51; then speed = 255 ..; if XX == 00; then speed == 0
 
-int moveMotors(String command)
+int move(String command)
 {
   String flcom = command.substring(0,3);
   String frcom = command.substring(4,7);
@@ -41,16 +53,67 @@ int flip(String command)
   return 1;
 }
 
+void parse(int x)
+{
+  if (x == 0xFD00FF)
+  {
+    Serial.println("Button pressed!");
+    if (flipping == true)
+    {
+      Serial1.println("stopflip");
+      Serial.println("stopflip");
+      flipping = false;
+    }
+    else
+    {
+      Serial1.println("flip");
+      Serial.println("flip");
+      flipping = true;
+    }
+  }
+
+  if (x == 0xFD807F)
+  {
+    move("151 151 151 151"); // Forwards
+  }
+
+  if (x == 0xFD906F)
+  {
+    move("251 251 251 251"); // Backwards
+  }
+
+  if (x == 0xFD20DF)
+  {
+    move("235 135 235 135"); // Counter-clockwise
+  }
+
+  if (x == 0xFDA05F)
+  {
+    move("135 235 135 235"); // Clockwise
+  }
+
+  if (x == 0xFD40BF)
+  {
+    move("000 000 000 000");
+  }
+
+}
+
+
 void setup()
 {
-Particle.function("move", moveMotors);
+Particle.function("move", move);
 Particle.function("flip", flip);
 Serial1.begin(9600);
 Serial.begin(9600);
-
+irrecv.enableIRIn(); // Start the receiver
 }
 
 void loop()
 {
-
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    parse(results.value);
+    irrecv.resume(); // Receive the next value
+  }
 }
